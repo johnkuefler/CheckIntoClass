0604d56a7cb21e2079c9fe09b4afc574d58a6678
John Kuefler <john.kuefler@limelightmarketing.com>
2023-12-31 15:01:06 -0600

    Added basic auth middleware

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+export { default } from "next-auth/middleware"
+
+// todo: customize this
+
+// See "Matching Paths" below to learn more
+export const config = {
+ // matcher: '/api/institutions/:path*',
+}
