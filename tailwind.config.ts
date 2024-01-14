ca3b3266f6730635d34e0660fd75c4bbdc03b45a
John Kuefler <john.kuefler@limelightmarketing.com>
2024-01-14 13:49:40 -0600

    added daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import daisyui from 'daisyui'
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 }
 export default config
