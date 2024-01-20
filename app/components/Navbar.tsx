8312f2ecb2d3f6248798bcd2e6592ef89d6f80ae
John Kuefler <john.kuefler@limelightmarketing.com>
2024-01-20 13:11:11 -0600

    Set up institutions crud grid

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Check Into Class</a>
+        <a href="/" className="btn btn-ghost text-xl">Check Into Class</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li><a>Manage Courses</a></li>
-          <li><a>Admin</a></li>
+          <li><a href="/">Manage Courses</a></li>
+          <li><a href="/admin">Admin</a></li>
           <li><a>Account</a></li>
           <li><LogoutButton/></li>
         </ul>
