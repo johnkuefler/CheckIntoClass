b5b9c9dcbc1b95f507504a6f5b34d246072e2726
Parker B <phburns@gus.pittstate.edu>
2024-01-31 11:06:40 -0600

    started the admin page links and put them in a list, still needs bullet points

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,13 @@
 const AdminPage = () => {
   
   return (
-    <div>
-      <a href="/admin/institutions">Manage Institutions</a>
-    </div>
+    <ul>
+      <li><a className="underline" href="">Manage Users</a></li>
+      <li><a className="underline" href="">Manage Departments</a></li>
+      <li><a className="underline" href="">Data Exports</a></li>
+      <li><a className="underline" href="/admin/institutions">Manage Institutions</a></li>
+      <li><a className="underline" href="">Delete Courses</a></li>
+    </ul>
   );
 };
 
