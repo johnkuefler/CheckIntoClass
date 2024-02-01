ca9dfa734fc7b494105564910a3bb4878a3b85b4
Parker B <phburns@gus.pittstate.edu>
2024-02-01 10:05:01 -0600

    un-blued the bullet points

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,12 @@
 const AdminPage = () => {
   
   return (
-    <ul className="list-disc text-cyan-400 mx-auto">
-      <li><a className="underline " href="#">Manage Users</a></li>
-      <li><a className="underline" href="#">Manage Departments</a></li>
-      <li><a className="underline" href="#">Data Exports</a></li>
-      <li><a className="underline" href="/admin/institutions">Manage Institutions</a></li>
-      <li><a className="underline" href="#">Delete Courses</a></li>
+    <ul className="list-disc mx-auto">
+      <li><a className="underline text-cyan-400" href="#">Manage Users</a></li>
+      <li><a className="underline text-cyan-400" href="#">Manage Departments</a></li>
+      <li><a className="underline text-cyan-400" href="#">Data Exports</a></li>
+      <li><a className="underline text-cyan-400" href="/admin/institutions">Manage Institutions</a></li>
+      <li><a className="underline text-cyan-400" href="#">Delete Courses</a></li>
     </ul>
   );
 };
