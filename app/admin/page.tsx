f646cdcac9762facff4ff6cf44ffb6d156c3407a
Jonas Garibay <56215286+BigJonas@users.noreply.github.com>
2024-02-01 08:44:09 -0600

    Added bullet points to admin link list

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,12 @@
 const AdminPage = () => {
   
   return (
-    <ul>
-      <li><a className="underline" href="">Manage Users</a></li>
-      <li><a className="underline" href="">Manage Departments</a></li>
-      <li><a className="underline" href="">Data Exports</a></li>
+    <ul className="list-disc">
+      <li><a className="underline" href="#">Manage Users</a></li>
+      <li><a className="underline" href="#">Manage Departments</a></li>
+      <li><a className="underline" href="#">Data Exports</a></li>
       <li><a className="underline" href="/admin/institutions">Manage Institutions</a></li>
-      <li><a className="underline" href="">Delete Courses</a></li>
+      <li><a className="underline" href="#">Delete Courses</a></li>
     </ul>
   );
 };
