ad53f2c743f450b6093665bc38caa9916b4ab203
John Kuefler <john.kuefler@limelightmarketing.com>
2024-02-27 08:16:01 -0600

    Added institutions dropdown Added userId to course

---
+++ app/components/Dropdowns/InstitutionsDropdown.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { use, useEffect, useState } from "react";
+
+const InstitutionsDropdown = () => {
+    const [institutions, setInstitutions] = useState([]);
+
+    useEffect(() => {
+        const fetchInstitutions = async () => {
+            const response = await fetch("/api/institutions");
+            const data = await response.json();
+            setInstitutions(data);
+        };
+
+        fetchInstitutions();
+    }, []);
+
+    return (
+        <select className="select select-bordered w-full max-w-xs">
+            {institutions.map((institution) => (
+                <option key={institution.id} value={institution.id}>
+                    {institution.name}
+                </option>
+            ))}
+        </select>
+    );
+}
+
+export default InstitutionsDropdown;
