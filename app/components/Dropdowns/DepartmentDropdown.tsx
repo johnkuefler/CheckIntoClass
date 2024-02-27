15d79338ad53ddbe21120f167b426d133839c451
Quintin1999 <qkramer@gus.pittstae.edu>
2024-02-27 13:30:49 -0600

    added dropdown for  Department

---
+++ app/components/Dropdowns/DepartmentDropdown.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { use, useEffect, useState } from "react";
+
+const DepartmentDropdown = () => {
+    const [departments, setDepartments] = useState([]);
+
+    useEffect(() => {
+        const fetchDepartments = async () => {
+            const respnose = await fetch ("/api/departments");
+            const data = await response.json();
+            setDepartments(data);
+        };
+
+        fetchDepartments();
+    },[]);
+
+    return(
+        <select
+            className = "select select-bordered w-full max-w-xs"
+            aria-label="Select Department"
+        >
+            {departments.map((department) => (
+                <option key={department.id} value={department.id}>
+                    {department.name}
+                </option>
+            ))}
+        </select>
+    );
+};
+
+export default DepartmentDropdown;
