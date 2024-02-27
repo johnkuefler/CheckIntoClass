822d001a63414ee120ca207d2b3756fe749055f8
hitrgiz87 <smithg7345@gmail.com>
2024-02-27 09:05:02 -0600

    created and tested user dropdown

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,7 +6,7 @@ const AdminPage = () => {
       <h2 className="text-2xl font-bold mt-2">Pittsburg State University</h2>
       <hr className="my-5" />
     <ul className="list-disc ml-5 mt-10">
-      <li><a className="underline text-cyan-400" href="#">Manage Users</a></li>
+      <li><a className="underline text-cyan-400" href="/admin/users">Manage Users</a></li>
       <li><a className="underline text-cyan-400" href="#">Manage Departments</a></li>
       <li><a className="underline text-cyan-400" href="#">Data Exports</a></li>
       <li><a className="underline text-cyan-400" href="/admin/institutions">Manage Institutions</a></li>
