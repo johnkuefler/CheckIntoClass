d584e583b5f2c06d68406bbcd0ab3e4403686d37
philbadcode <phillip.hail@yahoo.com>
2024-02-29 08:27:51 -0600

    added POST delegation

--- app/api/delegations/route.ts
+++ app/api/delegations/route.ts
@@ -5,3 +5,12 @@ export async function GET(request: NextRequest) {
   const delegation = await prisma.delegation.findMany();
   return NextResponse.json(delegation);
 }
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const { name, courseId, userId } = data;
+  const delegation = await prisma.delegation.create({
+    data: { name, courseId, userId },
+  });
+  return NextResponse.json(delegation);
+}
