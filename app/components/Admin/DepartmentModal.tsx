421cd35b3c1c5b6961f8c420ad6873f4dba43b65
Parker B <phburns@gus.pittstate.edu>
2024-03-28 08:34:35 -0500

    Added labels to the department model

--- app/components/Admin/DepartmentModal.tsx
+++ app/components/Admin/DepartmentModal.tsx
@@ -18,10 +18,19 @@ const DepartmentModal = ({ department, onSave, onClose, isEditMode }) => {
     return (
       <div className="modal modal-open">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">{isEditMode ? 'Edit' : 'Add'} Department</h3>
+          <h3 className="font-bold text-2xl">{isEditMode ? 'Edit' : 'Add'} Department</h3>
+          <div className="label -mb-2">
+            <span className="label-text">Department Name</span>
+          </div>
           <input type="text" placeholder="Name" className="input input-bordered w-full my-2" value={name} onChange={(e) => setName(e.target.value)} />
+          <div className="label -mb-2">
+            <span className="label-text">Department Code</span>
+          </div>
           <input type="text" placeholder="Code" className="input input-bordered w-full my-2" value={code} onChange={(e) => setCode(e.target.value)} />
-          <InstitutionsDropdown onInstitutionChange={handleDropdownChange}/>
+            <div className="label">
+              <span className="label-text">Institution</span>
+            </div>
+            <InstitutionsDropdown onInstitutionChange={handleDropdownChange}/>
           <div className="modal-action">
             <button onClick={handleSubmit} className="btn btn-primary">Save</button>
             <button onClick={onClose} className="btn">Cancel</button>
